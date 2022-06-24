0e6c471815159e1226521cf0a68a458061325b15
dtipei <tipei_denis@yahoo.com>
2022-06-24 00:49:10 -0700

    created timer

--- assets/script.js
+++ assets/script.js
@@ -9,5 +9,47 @@ var questions = [
         choices: ["Document object model", "Data object model", "Document oriented model", "Data oriented model"],
         answer: "Document oriented model",
 
+    },
+    {
+        title: "What language defines the behavior of a web page?",
+        choices: ["HTML", "CSS", "XML", "Java Script"],
+        answer: "Java Script",
+    },
+    {
+        title: "jQuery library is a _____ file",
+        choices: [".html", ".XML", ".js", ".css"],
+        answer: ".js",
+    },
+    {
+        title: "The .each method is the more convenient form of _____ loop.",
+        choices: ["do while", "for", "for each", "None of these"],
+        answer: "for",
     }
 ]
+
+var score = 0;
+var questionList = 0;
+var time = document.querySelector("#time");
+var timer = document.querySelector("questions");
+var wrapper = document.querySelector("wrapper");
+
+//create timer
+var secondsLeft = 80;
+var holdInterval = 0;
+var penalty = 5;
+var ulCreate = document.createElement("ul");
+
+timer.addEventListener("click", function () {
+    if (holdInterval === 0) {
+        holdInterval = setInterval(function () {
+            secondsLeft--;
+            time.textContent = "Time Left: " + secondsLeft;
+                if (secondsLeft <= 0) {
+                clearInterval(holdInterval);
+                allDone();
+                time.textContent = "Time's up!";
+            }
+        }, 1000);
+    }
+    render(questionList);
+});
