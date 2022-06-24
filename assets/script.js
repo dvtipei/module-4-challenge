e2989863701369a4e277db3d60e9da13c5801e71
dtipei <tipei_denis@yahoo.com>
2022-06-23 22:03:44 -0700

    started javascript file

---
+++ assets/script.js
@@ -0,0 +1,13 @@
+var questions = [
+    {
+        title: "What does HTML stand for?",
+        choices: ["Hyper Trainer Marking Language", "Hyper Text Marketing Language", "Hyper Text Markup Language", "Hyper Text Markup Leveler"],
+        answer: "Hyper Text Markup Language",
+    },
+    {
+        title: "DOM stands for:",
+        choices: ["Document object model", "Data object model", "Document oriented model", "Data oriented model"],
+        answer: "Document oriented model",
+
+    }
+]
